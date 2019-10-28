4ac69d00c2b93a317c12f7735f2a6de036d34997
Diogo <45275502+perkier@users.noreply.github.com>
2019-10-28 23:39:03 +0000

    Update Data_Reader.py

--- Kaggle/London_Energy/Data_Reader.py
+++ Kaggle/London_Energy/Data_Reader.py
@@ -96,14 +96,61 @@ def transform_df(df, name):
     return df
 
 
-def looping_all_db(df):
+def reduce_mem_usage(df, verbose=True):
 
-    pass
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+
+    for col in df.columns:
+
+        col_type = df[col].dtypes
+
+        if col_type in numerics:
+
+                c_min = df[col].min()
+                c_max = df[col].max()
+
+                if str(col_type)[:3] == 'int':
+
+                    if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        df[col] = df[col].astype(np.int8)
+
+                    elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                        df[col] = df[col].astype(np.int16)
+
+                    elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                        df[col] = df[col].astype(np.int32)
+
+                    elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                        df[col] = df[col].astype(np.int64)
+
+                else:
+
+                    if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                        df[col] = df[col].astype(np.float16)
+
+                    elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                        df[col] = df[col].astype(np.float32)
+
+                    else:
+                        df[col] = df[col].astype(np.float64)
+
+        end_mem = df.memory_usage().sum() / 1024**2
+
+        if verbose:
+
+            print('Mem. usage decreased from {:5.2f}Mb  to {:5.2f}Mb ({:.1f}% reduction)'.format(start_mem, end_mem, 100 * (start_mem - end_mem) / start_mem))
+
+        return df, start_mem, end_mem
 
 
 def main():
 
     see_all()
+
+    start_mem_tot = 0
+    end_mem_tot = 0
 
     for i in range(112):
 
@@ -111,9 +158,18 @@ def main():
 
         block_0_df = csv_func(name_block)
 
+        block_0_df, start_mem, end_mem = reduce_mem_usage(block_0_df, verbose=True)
+
         block_0_df = transform_df(block_0_df, name_block)
 
         print(i)
+        print('\n')
+
+        start_mem_tot += start_mem
+        end_mem_tot += end_mem
+
+    print('Total Mem. usage decreased from {:5.2f} Mb  to {:5.2f}Mb ({:.1f}% reduction)'.format(start_mem_tot,
+                                                                                                end_mem_tot, 100 * (start_mem_tot - end_mem_tot) / start_mem_tot))
 
 
 if __name__ == "__main__":
